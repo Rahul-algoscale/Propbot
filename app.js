0ed0795c1c0a1e7a2c0687f20c214cd31ac527b6
Rahul Kumar Mandal <rahul.mandal@algoscale.com>
2018-07-17 12:30:19 +0530

    done with initial views

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const express = require('express');
+const path = require('path');
 
+//Init App
 const app = express();
 
+// Load View Engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+// Home Route
 app.get('/', function(req, res) {
-    res.send('Hello World');
+    res.render('index');
 });
 
+
+// Start  Server
 app.listen(3000, function(){
-  console.log('Server started on port 3000..')
+  console.log('Server started on port 3000...')
 });
